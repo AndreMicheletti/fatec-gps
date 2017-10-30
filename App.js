059139a3d774688078eb5fc28da473b1c6d3fbe8
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2017-10-30 07:42:55 -0200

    adding modal and new markers

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Modal, Text, Button, View, Dimensions, StyleSheet } from 'react-native';
+import { Modal, Text, Button, View, StyleSheet, ScrollView } from 'react-native';
 import MapView from 'react-native-maps';
 import ActionButton from 'react-native-action-button'; // https://github.com/mastermoo/react-native-action-button
 
+import interestPoints from './data/markers'
+
 // marker's vars
 var markerTitle = "Secretaria"
-var markerDescription = "Edifício Santiago - Térreo"
+var markerDescription = ""
 var markerColor = "rgb(250, 255, 0)"
 
 const fatecRegion = {
@@ -14,214 +16,74 @@ const fatecRegion = {
   latitudeDelta: 0.002,
   longitudeDelta: 0.002,
 }
-// ######################## MARKERS COORDINATES ########################
-// --------------- geral ---------------
-const biblioteca = {
-  latitude: -23.529888,
-  longitude: -46.632472,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const estagio = {
-  latitude: -23.530133,
-  longitude: -46.632507,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const informatica = {
-  latitude: -23.529836,
-  longitude: -46.632550,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const secretaria = {
-  latitude: -23.530148,
-  longitude: -46.632394,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-// ---------------- departamento geral ----------------
-const geral = {
-  latitude: -23.529875,
-  longitude: -46.632373,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-// --------------- outros departamentos ---------------
-const edificio = {
-  latitude: -23.529546,
-  longitude: -46.632362,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const eletronica = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const hidraulica = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const materiais = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const mecanica = {
-  latitude: -23.529546,
-  longitude: -46.632362,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const processos = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const projetos = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const secretariado = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const soldagem = { // arrumar
-  latitude: 0,
-  longitude: 0,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const tecnologia = {
-  latitude: -23.530131,
-  longitude: -46.632418,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-const turismo = {
-  latitude: -23.529546,
-  longitude: -46.632362,
-  latitudeDelta: 0.002,
-  longitudeDelta: 0.002,
-}
-
 
 export default class App extends Component<{}> {
+
   // Modal
   state = {
     modalVisible: false,
   }
+
   setModalVisible(visible) {
     this.setState({modalVisible: visible});
+  }
+
+  renderMarkers() {
+    return interestPoints.map((point) => {
+      return (
+        <MapView.Marker
+          key={point.title}
+          coordinate={point.coords}
+          title={point.title}
+          subtitle={point.subtitle}
+          pinColor={point.color}
+        />
+      );
+    });
   }
 
   render() {
     return (
       <View style={{flex: 1}}>
-      <MapView
-      style={styles.mapStyle}
-      initialRegion={fatecRegion}
-      region={fatecRegion}
-      scrollEnabled={false}
-      rotateEnabled={false}
-      zoomEnabled={false}
-      pitchEnabled={false}
-      minZoomLevel={15}
-      maxZoomLevel={25}
-      showsUserLocation={true}
-      showsMyLocationButton={false}
-      >
-      <MapView.Marker
-      coordinate={{
-        latitude: -23.529289,
-        longitude: -46.632550,
-        latitudeDelta: 0.002,
-        longitudeDelta: 0.002,
-      }}
-      title="Prédio A/B - Bloco B"
-      />
-      <MapView.Marker
-      coordinate={{
-        latitude: -23.529145,
-        longitude: -46.632323,
-        latitudeDelta: 0.002,
-        longitudeDelta: 0.002,
-      }}
-      title="Prédio A/B - Bloco B"
-      />
-      <MapView.Marker
-      coordinate={{
-        latitude: -23.529987,
-        longitude: -46.632485,
-        latitudeDelta: 0.002,
-        longitudeDelta: 0.002,
-      }}
-      title="Edificio Santhiago"
-      />
-      <MapView.Marker
-      coordinate={{
-        latitude: -23.530069,
-        longitude: -46.632816,
-        latitudeDelta: 0.002,
-        longitudeDelta: 0.002,
-      }}
-      title="Centro Acadêmico"
-      />
-      <MapView.Marker
-      coordinate={{
-        latitude: -23.528951,
-        longitude: -46.633147,
-        latitudeDelta: 0.002,
-        longitudeDelta: 0.002,
-      }}
-      title="Etesp"
-      />
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={fatecRegion}
+          region={fatecRegion}
+          scrollEnabled={false}
+          rotateEnabled={false}
+          zoomEnabled={false}
+          pitchEnabled={false}
+          minZoomLevel={15}
+          maxZoomLevel={25}
+          showsUserLocation={true}
+          showsMyLocationButton={false}>
+            {/* Render Markers */}
+            {this.renderMarkers()}
+        </MapView>
 
-      // variable marker
-      <MapView.Marker
-      coordinate= { secretaria }
-      title= markerTitle
-      description= markerDescription
-      color= markerColor
-      />
-      </MapView>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {this.setModalVisible(!this.state.modalVisible)}}>
 
-      <Modal
-      animationType="slide"
-      transparent={false}
-      visible={this.state.modalVisible}
-      onRequestClose={() => {this.setModalVisible(!this.state.modalVisible)}}
-      >
-      <View style={{marginTop: 22}}>
-      <View style= {styles.modal}>
-      <ScrollView>
-      <Text style= {styles.text}>Scroll me plz {"\n"}
-      If you like</Text>
-      <Text style= {styles.link}>React Native</Text>
-      <Text style= {styles.subtitulo}>SALAS ESPECIAIS</Text>
-      </ScrollView>
+          <View style={{marginTop: 22}}>
+            <View style= {styles.modal}>
+              <ScrollView>
+                <Text style= {styles.subtitulo}>SALAS ESPECIAIS</Text>
+              </ScrollView>
 
-      <Button
-      onPress={() => {this.setModalVisible(!this.state.modalVisible)} }
-      title="Fechar"
-      color="#841584"
-      />
-      </View>
-      </View>
-      </Modal>
+              <Button
+                onPress={() => {this.setModalVisible(!this.state.modalVisible)}}
+                style={styles.closeButtonStyle}
+                title="Fechar"
+                color="#841584"
+              />
+            </View>
+          </View>
+        </Modal>
 
-      <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => this.setModalVisible(true)}>
-      </ActionButton>
+        <ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => this.setModalVisible(true)} />
       </View>
     );
   }
@@ -235,17 +97,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-    color: 'black',
-    fontSize: 30,
-  },
-  link:{
-    color: 'blue',
-    fontSize: 30,
-  },
   subtitulo:{
     color: 'black',
     fontWeight: 'bold',
     fontSize: 30,
+  },
+  closeButtonStyle: {
+    padding: 5,
+    borderRadius: 5
   }
 })
---
+++ data/markers.js
@@ -0,0 +1,240 @@
+import React from 'react'
+
+// ######################## MARKERS COORDINATES ########################
+
+// --------------- geral ---------------
+export default interestPoints = [
+  {
+    title: "Prédio A/B - Bloco A",
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529289,
+      longitude: -46.632550,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    }
+  },
+  {
+    title: "Prédio A/B - Bloco B",
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529145,
+      longitude: -46.632323,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    }
+  },
+  {
+    title: "Edificio Santhiago",
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529987,
+      longitude: -46.632485,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    }
+  },
+  {
+    title: "Centro Acadêmico",
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.530069,
+      longitude: -46.632816,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    }
+  },
+  {
+    title: "Etesp",
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.528951,
+      longitude: -46.633147,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    }
+  },
+  {
+    title: 'biblioteca',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529888,
+      longitude: -46.632472,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  {
+    title: 'estagio',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.530133,
+      longitude: -46.632507,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  {
+    title: 'informatica',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529836,
+      longitude: -46.632550,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  {
+    title: 'Secretaria',
+    subtitle: 'Edifício Santiago - Térreo',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.530148,
+      longitude: -46.632394,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  // ---------------- departamento geral ----------------
+  {
+    title: 'geral',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529875,
+      longitude: -46.632373,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  // --------------- outros departamentos ---------------
+  {
+    title: 'edificio',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529546,
+      longitude: -46.632362,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'eletronica',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'hidraulica',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'materiais',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  {
+    title: 'mecanica',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529546,
+      longitude: -46.632362,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'processos',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'projetos',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'secretariado',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  { // arrumar
+    title: 'soldagem',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  {
+    title: 'tecnologia',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.530131,
+      longitude: -46.632418,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    },
+  },
+  {
+    title: 'turismo',
+    subtitle: '',
+    color: 'rgba(255, 255, 0)',
+    coords: {
+      latitude: -23.529546,
+      longitude: -46.632362,
+      latitudeDelta: 0.002,
+      longitudeDelta: 0.002,
+    }
+  }
+]
