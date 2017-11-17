e9f74e2bb657f98ff93eaa8c96c2ba726729a0b0
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2017-11-17 14:30:51 -0200

    include src folder

---
+++ src/data/mapSettings.js
@@ -0,0 +1,11 @@
+export default mapSettings = {
+  showsPointsOfInterest: false,
+  scrollEnabled: false,
+  rotateEnabled: false,
+  zoomEnabled: false,
+  pitchEnabled: false,
+  minZoomLevel: 15,
+  moveOnMarkerPress: false,
+  provider: "google",
+  cacheEnabled: true
+};
